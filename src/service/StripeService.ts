8ab7ba9d70f2ee0f8e7d303ace9c8a4905a76b5f
Your Name <javierconpec@gmail.com>
2025-05-16 17:59:24 -0500

    .

--- src/service/StripeService.ts
+++ src/service/StripeService.ts
@@ -1,16 +1,20 @@
-
+import axios from "axios";
 import type { StripeSessionResponse } from "../interface/IStripe";
 
 export const createStripeSession = async (priceId: string): Promise<StripeSessionResponse> => {
-  const res = await fetch("https://contacto-19h2.onrender.com/api/create-checkout-session", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ priceId }),
-  });
+  try {
+    const response = await axios.post<StripeSessionResponse>(
+      "https://contacto-19h2.onrender.com/api/checkout",
+      { priceId },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  if (!res.ok) {
+    return response.data;
+  } catch (error) {
     throw new Error("Error al crear la sesión de Stripe");
   }
-
-  return res.json();
 };
